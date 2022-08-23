b950f073887aed2e4fa1075ee7a58ac659fc5f69
Breno Colaço <brenocolaco@unifor.br>
2022-08-23 17:49:35 -0300

    Mousedown Event

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Button from "../Button";
 
 import { ButtonContainer, ModalBackground, ModalContainer } from "./styles";
@@ -9,9 +9,26 @@ type Props = {
   modal: Boolean;
 };
 function Modal({ text, setModal, modal, onClick }: Props) {
+  let ref = useRef<HTMLHeadingElement>(null);
+  useEffect(() => {
+    let handler = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        !ref.current.contains(event.target as HTMLDivElement)
+      ) {
+        setModal(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, []);
+
   return (
     <ModalBackground>
-      <ModalContainer>
+      <ModalContainer ref={ref}>
         <label>{text}</label>
         <ButtonContainer>
           <Button
