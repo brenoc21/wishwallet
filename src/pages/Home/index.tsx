57f93ef211726ebf44292825c898802dafcbba28
Breno Colaço <brenocolaco@unifor.br>
2022-08-24 23:20:56 -0300

    starting unit tests

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import {
   TitleRow,
-  
   TokenContainer,
   TokenTable,
   TokenHeader,
   TokenBody,
 } from "./styles";
-
 
 import EditIcon from "../../assets/edit.svg";
 import Button from "../../components/Button/index";
@@ -17,65 +15,68 @@ import Layout from "../../components/Layout/index";
 import WishLogo from "../../components/WishLogo";
 export default function Home() {
   const { data } = useData();
-  
+
   const navigate = useNavigate();
-  
+
   return (
     <Layout>
       <TitleRow>
-      <WishLogo/>
+        <WishLogo />
         <Button
-        type="button"
+          type="button"
           color={"var(--purple)"}
           text={"Add Token"}
           onClick={() => navigate("/addtoken")}
         />
       </TitleRow>
       <TokenContainer>
-       {data.length > 0 ? <TokenTable>
-          <TokenHeader>
-            <tr><td className="editBox"/><td>Tokens</td><td>Balance</td></tr>
-            
-          </TokenHeader>
-          <TokenBody>
-            {
-              
-              data.map((token) => {
+        {data.length > 0 ? (
+          <TokenTable>
+            <TokenHeader>
+              <tr>
+                <td className="editBox" />
+                <td>Tokens</td>
+                <td>Balance</td>
+              </tr>
+            </TokenHeader>
+            <TokenBody data-testId={"tokenBody"}>
+              {data.map((token) => {
                 return (
                   <React.Fragment>
-                  <tr>
-                    <td
-                      className="editBox"
-                      onClick={() => {
-                        navigate(`/${token._id}`)}}
-                    >
-                      <img alt="EditIcon" src={EditIcon} />
-                    </td>
+                    <tr>
+                      <td
+                        className="editBox"
+                        onClick={() => {
+                          navigate(`/${token._id}`);
+                        }}
+                      >
+                        <img alt="EditIcon" src={EditIcon} />
+                      </td>
 
-                    <td className="item">{token.name.toUpperCase()}</td>
-                    <td className="item">
+                      <td className="item">{token.name.toUpperCase()}</td>
+                      <td className="item">
                         {new Intl.NumberFormat("en-US", {
                           style: "currency",
-                        currency: "USD",
-                        currencyDisplay: "symbol",
-                      })
-                        .formatToParts(token.value)
-                        .map((p) =>
-                          p.type !== "literal" && p.type !== "currency"
-                            ? p.value
-                            : ""
-                        )
-                        .join("")}
-                    </td>
-                  </tr>
+                          currency: "USD",
+                          currencyDisplay: "symbol",
+                        })
+                          .formatToParts(token.value)
+                          .map((p) =>
+                            p.type !== "literal" && p.type !== "currency"
+                              ? p.value
+                              : ""
+                          )
+                          .join("")}
+                      </td>
+                    </tr>
                   </React.Fragment>
                 );
-              })
-             
-            }
-          </TokenBody>
-        </TokenTable> :  
-              <p>No token has been registered.</p>}
+              })}
+            </TokenBody>
+          </TokenTable>
+        ) : (
+          <p>No token has been registered.</p>
+        )}
       </TokenContainer>
     </Layout>
   );
